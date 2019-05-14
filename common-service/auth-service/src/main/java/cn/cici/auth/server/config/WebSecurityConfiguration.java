2a21e61a96d826dd1d8bf11dc4e9071bc9df56f3
xiehf319 <370693739@qq.com>
2019-05-14 22:54:55 +0800

    授权

--- common-service/auth-service/src/main/java/cn/cici/auth/server/config/WebSecurityConfiguration.java
+++ common-service/auth-service/src/main/java/cn/cici/auth/server/config/WebSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -49,6 +50,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .userDetailsService(userServiceDetail)
                 .passwordEncoder(passwordEncoder());
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
