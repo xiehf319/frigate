fce26f9dd4cff9bf8a61c8cf9861ea63f58c8741
xiehf319 <370693739@qq.com>
2019-06-20 23:08:21 +0800

    自定义扫描注解

---
+++ component/component-core/src/main/java/cn/cici/frigate/component/scanner/LogCollectorScannerRegistrar.java
@@ -0,0 +1,60 @@
+package cn.cici.frigate.component.scanner;
+
+import cn.cici.frigate.component.annotation.EnableLogCollector;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xiehf
+ * @date 2019/6/20 22:30
+ * @concat 370693739@qq.com
+ **/
+@Slf4j
+public class LogCollectorScannerRegistrar implements ImportBeanDefinitionRegistrar, ResourceLoaderAware, EnvironmentAware {
+
+
+    private ResourceLoader resourceLoader;
+
+    private Environment environment;
+
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata annotationMetadata, BeanDefinitionRegistry beanDefinitionRegistry) {
+
+        AnnotationAttributes attributes = AnnotationAttributes
+                .fromMap(annotationMetadata.getAnnotationAttributes(EnableLogCollector.class.getName()));
+
+        ClassPathLogCollectorScanner scanner = new ClassPathLogCollectorScanner(beanDefinitionRegistry);
+        if (resourceLoader != null) {
+            scanner.setResourceLoader(resourceLoader);
+        }
+        List<String> basePackages = new ArrayList<>();
+        for (String pkg: attributes.getStringArray("basePackages")) {
+            if (StringUtils.hasText(pkg)) {
+                basePackages.add(pkg);
+            }
+        }
+        scanner.doScan(StringUtils.toStringArray(basePackages));
+    }
+
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+}
