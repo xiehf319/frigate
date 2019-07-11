fc2857871b094866a476331a6fc7a9297b35ec95
xiehf319 <370693739@qq.com>
2019-07-11 23:48:14 +0800

    websocket

---
+++ common-service/message-service/src/main/java/cn/cici/frigate/msg/config/RedisConfig.java
@@ -0,0 +1,15 @@
+package cn.cici.frigate.msg.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xiehf
+ * @description: 类介绍:
+ * @date 2019/7/11 23:45
+ * @concat 370693739@qq.com
+ **/
+@Configuration
+//@ConditionalOnClass(JedisCluster.class)
+public class RedisConfig {
+}
---
+++ common-service/message-service/src/main/java/cn/cici/frigate/msg/enums/WebsocketChannelEnum.java
@@ -0,0 +1,49 @@
+package cn.cici.frigate.msg.enums;
+
+/**
+ * @author xiehf
+ * @description: 类介绍:
+ * @date 2019/7/11 23:40
+ * @concat 370693739@qq.com
+ **/
+public enum  WebsocketChannelEnum {
+
+    /**
+     * 点对点聊天
+     */
+    CHAT("", "/topic/reply"),
+
+    ;
+
+
+    WebsocketChannelEnum(String descrition, String subscribeUrl) {
+        this.descrition = descrition;
+        this.subscribeUrl = subscribeUrl;
+    }
+
+    /**
+     * 描述
+     */
+    private String descrition;
+
+    /**
+     * 客户端订阅的URL
+     */
+    private String subscribeUrl;
+
+    public String getDescrition() {
+        return descrition;
+    }
+
+    public void setDescrition(String descrition) {
+        this.descrition = descrition;
+    }
+
+    public String getSubscribeUrl() {
+        return subscribeUrl;
+    }
+
+    public void setSubscribeUrl(String subscribeUrl) {
+        this.subscribeUrl = subscribeUrl;
+    }
+}
