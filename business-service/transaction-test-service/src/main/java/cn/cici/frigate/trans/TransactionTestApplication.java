f35cf2e5963e4e4187d778668c38633ae7ad41b1
xiehf319 <370693739@qq.com>
2019-07-11 23:38:18 +0800

    整理代码

--- business-service/transaction-test-service/src/main/java/cn/cici/frigate/trans/TransactionTestApplication.java
+++ business-service/transaction-test-service/src/main/java/cn/cici/frigate/trans/TransactionTestApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class TransactionTestApplication {
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(TransactionTestApplication.class, args);
-//	}
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionTestApplication.class, args);
+	}
 
 }
--- common-service/message-service/src/main/java/cn/cici/frigate/msg/MessageServiceApplication.java
+++ common-service/message-service/src/main/java/cn/cici/frigate/msg/MessageServiceApplication.java
@@ -1,9 +1,19 @@
 package cn.cici.frigate.msg;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @description: 类介绍：
  * @createDate: 2019/7/11 17:32
  * @author: Heyfan Xie
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class MessageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageServiceApplication.class, args);
+    }
 }
