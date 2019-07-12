551c5fd81d8ee1df98625248076c493f3fb97970
xiehf319 <370693739@qq.com>
2019-07-12 18:25:58 +0800

    自定义userDetail

--- system-framework/api-gateway/src/main/java/cn/cici/frigate/gateway/GatewayApplication.java
+++ system-framework/api-gateway/src/main/java/cn/cici/frigate/gateway/GatewayApplication.java
@@ -2,13 +2,15 @@ package cn.cici.frigate.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * https://blueskykong.com/2017/12/10/integration/
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
